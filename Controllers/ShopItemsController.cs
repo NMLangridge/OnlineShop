586609bc96af6372cb48c10142c603ee8a86962c
Nathan Langridge <nathanlangridge@hotmail.com>
2021-01-12 15:48:26 +0000

    removed unnecessary using directives

--- Controllers/ShopItemsController.cs
+++ Controllers/ShopItemsController.cs
@@ -1,8 +1,6 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ShopApi.Models;
@@ -42,7 +40,6 @@ namespace ShopApi.Controllers
         }
 
         // PUT: api/ShopItems/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
         public async Task<IActionResult> PutShopItem(long id, ShopItem shopItem)
         {
