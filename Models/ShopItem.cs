f6fa47d1ed72d2d1f4450f6cfa5c1c788a737b80
Nathan Langridge <nathanlangridge@hotmail.com>
2021-01-12 15:28:55 +0000

    amended ShopItem

--- Models/ShopItem.cs
+++ Models/ShopItem.cs
@@ -3,7 +3,8 @@ namespace ShopApi.Models
     public class ShopItem
     {
         public long Id { get; set; }
+        public string ProductCode { get; set; }
         public string Name { get; set; }
-        public bool IsComplete { get; set; }
+        public string Price { get; set; }
     }
 }
