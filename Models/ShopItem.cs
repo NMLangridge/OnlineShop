f5b4c5c8ecd255d0329d6eafd45c07a8e2b2e007
Nathan Langridge <nathanlangridge@hotmail.com>
2021-01-12 11:09:17 +0000

    added Models folder containing ShopItem

---
+++ Models/ShopItem.cs
@@ -0,0 +1,9 @@
+namespace ShopApi.Models
+{
+    public class ShopItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
